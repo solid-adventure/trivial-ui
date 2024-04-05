af69687c5858fa77d04ee82a0e29909b941bb4ce
Elmer Vasquez <elfvasqu@ucsc.edu>
2024-04-05 12:07:04 -0700

    adding apiCall to constructor in permissions

--- src/models/Permissions.js
+++ src/models/Permissions.js
@@ -1,5 +1,8 @@
+import Session from './Session'
 export default class Permissions {
-    constructor() {}
+    constructor() {
+        const userPermissions = await Session.apiCall(`/users/${state.user.id}/permissions`)
+    }
     static setPermissions(permissions){
         this.update = permissions.update
     }
