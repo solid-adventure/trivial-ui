5b84fa40d0ee06487602e648b932a1facf229260
Elmer Vasquez <elfvasqu@ucsc.edu>
2024-04-18 13:44:18 -0700

    updated test and errors in Permission class

--- src/models/Permissions.js
+++ src/models/Permissions.js
@@ -1,86 +1,78 @@
 export default class Permissions {
+// Creating a Permissions instance:
+// const permissions = new Permissions()
 
-  // Creating a Permissions instance:
-  // const permissions = new Permissions()
+// Method: can()
+// Description:
+// Validates whether the user can perform specific actions on apps or organizations.
+// It loads permissions if they are not set and checks the provided ability, model, and args.
+// Args required are specified in validateOrgAbility() and validateAppAbility().
+// Returns:
+// A boolean indicating whether the user can perform the specified action.
+// Example usage:
+// - permissions.can('update', 'App', {appName: appName})
+// - permissions.can('removeUser', 'Org', {memberRole: memberRole, userRole: userRole, lastAdmin: lastAdmin})
 
-  // Method: load()
-  // Description:
-  // Load the user's permissions list by making an API call. The user's ID is required for the first call,
-  // and reused for subsequent calls unless a new one is specified.
-  // Returns: A promise that resolves with the user's permissions list.
-  // Example usage: permissions.load(userId) or permissions.load()
+// Note on validateAppAbility() and validateOrgAbility():
+// These methods are called in validate(), which is then invoked in can().
+// It's recommended to use can() to ensure the ability is assigned to an available/appropriate model.
 
-  // Method: can()
-  // Description:
-  // Validate app actions by checking ability, model, and args. Returns a boolean to verify that the user
-  // can perform the specified action. Args required are specified in validateOrgAbility() and validateAppAbility().
-  // Returns: A boolean indicating whether the user can perform the specified action.
-  // Example usage:
-  // - permissions.can('update', 'App', {appName: appName})
-  // - permissions.can('removeUser', 'Org', {memberRole: memberRole, userRole: userRole, lastAdmin: lasAdmin})
-
-  // Note:
-  // validateAppAbility() and validateOrgAbility() are called in can(). It's recommended to use can() to ensure
-  // that you are committing an ability towards an available/appropriate model
+// Note on removeMember ability: 
+// In the current implementation, an admin cannot remove themselves from the organization
+// if they are the last admin in the group.
 
   constructor(Session, user) {
-    this.Session = Session
-    this._userId = user?.id
+    this.Session = Session;
+    this._userId = user?.id;
     if (!this._userId || !this.Session) {
       throw new Error("User and Session is required.");
     }
   }
 
   load() {
-    this._permissions = this._permissions || this.Session.apiCall(
-      `/users/${this._userId}/permissions`
-    );
-    return this._permissions
+    this._permissions =
+      this._permissions ||
+      this.Session.apiCall(`/users/${this._userId}/permissions`);
+    return this._permissions;
   }
 
-  reset(){
-    this._permissions = null
+  reset() {
+    this._permissions = null;
   }
 
   can(ability, model, args) {
-    return this.load().then((permissions) => {
-      return this.validate(permissions, ability, model, args)
-    })
-    .catch((error) => {
-      return false
-      console.error(error)
-    })
+    return this.load()
+      .then((permissions) => {
+        return this.validate(permissions, ability, model, args);
+      })
+      .catch((error) => {
+        console.error(error);
+        return false;
+      });
   }
 
-  validate(permissions, ability, model, args){
-    if (!permissions) {
-      console.error("Permissions have not been initialized.");
-      return false;
-    }
+  validate(permissions, ability, model, args) {
     if (model === "App") {
       return this.validateAppAbility(permissions, ability, args);
     } else if (model === "Org") {
       return this.validateOrgAbility(ability, args);
     } else {
-      console.error(`Model: ${model} not found`);
-      return false;
+      throw new Error(`Model: ${model} not found`);
     }
   }
 
-  validateAppAbility(permissions, ability, { appName: appName}) {
-    try {
-      let abilities = ['destroy', 'update', 'grant', 'transfer'];
-      if (abilities.includes(ability)) {
+  validateAppAbility(permissions, ability, { appName: appName }) {
+    let abilities = ["destroy", "update", "grant", "transfer"];
+    if (abilities.includes(ability)) {
+      if (!appName) {
+        throw new Error("appName is required in args");
+      } else {
         let appNames = permissions[ability]?.app_names;
         let response = appNames.includes(appName);
-        return response
-      } else {
-        console.error(`Ability: ${ability} not found in App permissions`);
-        return false;
+        return response;
       }
-    } catch (error) {
-      console.error(error);
-      return false;
+    } else {
+      throw new Error(`Ability: ${ability} not found in App permissions`);
     }
   }
 
@@ -88,19 +80,23 @@ export default class Permissions {
     ability,
     { memberRole: memberRole, userRole: userRole, lastAdmin: lastAdmin }
   ) {
-    try {
-      if (ability === "removeMember") {
+    if (ability === "removeMember") {
+      if (!memberRole || !userRole || lastAdmin === undefined) {
+        throw new Error(
+          "memberRole, userRole, and lastAdmin are required in args"
+        );
+      } else {
         if (userRole === "admin") {
-          return memberRole !== "admin" || !lastAdmin;
+          if (memberRole === "member") {
+            return true;
+          } else {
+            return memberRole === "admin" && !lastAdmin;
+          }
         }
         return false;
-      } else {
-        console.error(`Ability: ${ability} not found in Org permissions`);
-        return false;
       }
-    } catch (error) {
-      console.error(error);
-      return false;
+    } else {
+      throw new Error(`Ability: ${ability} not found in Org permissions`);
     }
   }
 }
--- test/lib/Permissions.js
+++ test/lib/Permissions.js
@@ -12,6 +12,7 @@ let mockPermissionsResponse = {
       "0b07af48aa8991",
       "5fbd0cef5d9306",
       "c4c270d211fea2",
+      "5b83e11ab9ff8e"
     ],
     credential_set_ids: [],
   },
@@ -21,6 +22,7 @@ let mockPermissionsResponse = {
       "0b07af48aa8991",
       "5fbd0cef5d9306",
       "c4c270d211fea2",
+      "a2dcf60ef9a7c3"
     ],
     credential_set_ids: [],
   },
@@ -30,6 +32,7 @@ let mockPermissionsResponse = {
       "0b07af48aa8991",
       "5fbd0cef5d9306",
       "c4c270d211fea2",
+      "fb2e09a5c381d6"
     ],
     credential_set_ids: [],
   },
@@ -39,6 +42,7 @@ let mockPermissionsResponse = {
       "0b07af48aa8991",
       "5fbd0cef5d9306",
       "c4c270d211fea2",
+      "8dfe7a96b0b5e4"
     ],
     credential_set_ids: [],
   },
@@ -47,62 +51,143 @@ let mockPermissionsResponse = {
 describe("Permissions", () => {
   let permissions;
   let sessionMock;
-  let consoleErrorSpy;
   let model;
   let ability;
   let appName;
-  const userId = 2;
+  let result;
 
   sessionMock = {
     apiCall: sinon.stub().resolves(mockPermissionsResponse),
   };
 
   let mockUser = {
-    id: "1"
-  }
-  
-  describe("load", () => {
+    id: "1",
+  };
+
+  describe("new Permission", () => {
     it("should throw an error if user ID or Session is not provided", async () => {
-      expect(() => new Permissions(sessionMock)).to.throw("User and Session is required.");
+      expect(() => new Permissions(sessionMock)).to.throw(
+        "User and Session is required."
+      );
       expect(() => new Permissions()).to.throw("User and Session is required.");
     });
+  });
 
-    it("should load a list of Permissions when userId is provided", async () => {
-      permissions = new Permissions(sessionMock, mockUser);
-      expect(permissions._permissions).to.not.exist;
-      await permissions.load(userId);
-      expect(permissions._permissions).to.exist;
+  describe("can-org", () => {
+    it("should return false when userRole is admin, memberRole is admin and lastAdmin is true ", async () => {
+      result = await permissions.can("removeMember", "Org", { memberRole: "admin", userRole: "admin", lastAdmin: true });
+      expect(result).to.be.false;
+
+    });
+    
+    it("should return true when userRole is admin, memberRole is admin and lastAdmin is true ", async () => {
+      result = await permissions.can("removeMember", "Org", { memberRole: "member", userRole: "admin", lastAdmin: true });
+      expect(result).to.be.true;
+    });
+
+    it("should return true when userRole is admin and lastAdmin is false", async () => {
+      result = await permissions.can("removeMember", "Org", { memberRole: "admin", userRole: "admin", lastAdmin: false });
+      expect(result).to.be.true;
+      result = await permissions.can("removeMember", "Org", { memberRole: "member", userRole: "admin", lastAdmin: false });
+      expect(result).to.be.true;
+    });
+
+    // userRole member automatically defaults to false before checking memberRole and lastAdmin
+    it("should return false when userRole is member", async () => {
+      result = await permissions.can("removeMember", "Org", { memberRole: "admin", userRole: "member", lastAdmin: false });
+      expect(result).to.be.false;
+      result = await permissions.can("removeMember", "Org", { memberRole: "member", userRole: "member", lastAdmin: false });
+      expect(result).to.be.false;
+      result = await permissions.can("removeMember", "Org", { memberRole: "admin", userRole: "member", lastAdmin: true });
+      expect(result).to.be.false;
+      result = await permissions.can("removeMember", "Org", { memberRole: "member", userRole: "member", lastAdmin: true });
+      expect(result).to.be.false;
+    });
+
+  })
+
+  beforeEach(() => {
+    permissions = new Permissions(sessionMock, mockUser);
+  });
+
+  describe("can-app", () => {
+
+    it("should return true when appropriate ability, model, and args are inputted", async () => {
+      appName = "8dfe7a96b0b5e4";
+      result = await permissions.can("update", "App", { appName: appName });
+      expect(result).to.be.true;
+      appName = "fb2e09a5c381d6"
+      result = await permissions.can("transfer", "App", { appName: appName });
+      expect(result).to.be.true;
+      appName = "5b83e11ab9ff8e"
+      result = await permissions.can("destroy", "App", { appName: appName });
+      expect(result).to.be.true;
+      appName = "a2dcf60ef9a7c3"
+      result = await permissions.can("grant", "App", { appName: appName });
+      expect(result).to.be.true;
+    });
+
+    it("should return false when app doesn't exist in users permissions", async () => {
+      appName = "i3bd0aefud13s6"; // random app name
+      result = await permissions.can("update", "App", { appName: appName });
+      expect(result).to.be.false;
     });
   });
 
-  describe("can", () => {
-    beforeEach(() => {
-      permissions = new Permissions(sessionMock, mockUser);
-      consoleErrorSpy = sinon.spy(console, "error");
-    });
-  
-    afterEach(() => {
-      consoleErrorSpy.restore();
-    });
-
-    it("should throw an error if model is not available", async () => {
-      model = "unknown";
-      appName = "i3bd0aefud13s6";
-      await permissions.can("update", model, { appName: appName });
-
-      expect(consoleErrorSpy.calledWith(`Model: ${model} not found`)).to.be
-        .true;
+  describe("validateModelAndAbility", () => {
+    it("should throw error when model is not found", async () => {
+      appName = "af6abd07ecdcdd";
+      try {
+        permissions.validate(mockPermissionsResponse, "update", "unknown", { appName: appName });
+        expectt.fail("Validation model fail did not pass");
+      } catch (error) {
+        expect(error.message).to.equal("Model: unknown not found");
+      }
     });
 
-    it("should throw an error if ability is not available for App", async () => {
-      ability = "unknown";
-      appName = "i3bd0aefud13s6";
-      await permissions.can("unknown", "App", { appName: appName });
-      expect(
-        consoleErrorSpy.calledWith(
-          `Ability: ${ability} not found in App permissions`
-        )
-      ).to.be.true;
+    it("should throw error when app ability is not found", async () => {
+      try {
+        appName = "af6abd07ecdcdd";
+        permissions.validateAppAbility(mockPermissionsResponse, "unknown", { appName: appName });
+        expectt.fail("Validation app ability fail did not pass");
+      } catch (error) {
+        expect(error.message).to.equal("Ability: unknown not found in App permissions");
+      }
+    });
+
+    it("should throw error when org ability is not found", async () => {
+      try {
+        permissions.validateOrgAbility("unknown", { memberRole: "admin", userRole: "member", lastAdmin: true });
+        expectt.fail("Validation org ability fail did not pass");
+      } catch (error) {
+        expect(error.message).to.equal("Ability: unknown not found in Org permissions");
+      }
     });
   });
+
+  describe("validateArgs", () => {
+    it("should throw an error if appropriate args are not set for validating org abilities", () => {
+      try {
+        permissions.validateOrgAbility("removeMember", {memberRole: undefined, userRole: undefined, lastAdmin: undefined});
+        expectt.fail("Validation for args fail did not pass");
+      } catch (error) {
+        expect(error.message).to.equal("memberRole, userRole, and lastAdmin are required in args");
+      }
+    })
+    it("should throw an error if appropriate args are not set for validating app abilities", () => {
+      try {
+        permissions.validateAppAbility(mockPermissionsResponse, "update", {appName: undefined});
+        expectt.fail("Validation for args fail did not pass");
+      } catch (error) {
+        expect(error.message).to.equal("appName is required in args");
+      }
+    })
+  })
+
+  describe("reset", () => {
+    it("should set _permissions to null when reset() is called", () => {
+      permissions.reset()
+      expect(permissions._permissions).to.equal(null);
+    })
+  })
 });
