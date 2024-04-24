b437483cd220661f145b433526966e6a9edbaa8d
GoranRadmanovic <goranradmanovic@gmail.com>
2024-04-24 19:26:08 +0200

    Creating new aside bar menu, reworking Breadcrumbs section, reworking main header section

---
+++ src/components/aside/Naviagation.vue
@@ -0,0 +1,24 @@
+<template>
+	<nav class="aside__nav">
+		<RouterLink v-for="(value, index) in menuItems" :key="index" :to="value.path" class="aside__nav__link">
+			<div>
+				<i :class="value.icon"></i>
+				<span>{{ value.title }}</span>
+			</div>
+		</RouterLink>
+	</nav>
+</template>
+
+<script setup>
+	import { RouterLink } from 'vue-router'
+	import { ref } from 'vue'
+
+	const menuItems = ref([
+		{ path: "/dashboard", title: 'Dashboard', icon: 'fa-solid fa-house' },
+		{ path: "/#", title: 'Sales', icon: 'fa-solid fa-sack-dollar' },
+		{ path: "/#", title: 'Inventory', icon: 'fa-solid fa-shirt' },
+		{ path: "/contract", title: 'Contracts', icon: 'fa-solid fa-file-pen' },
+		{ path: "/#", title: 'Transactions', icon: 'fa-solid fa-book-open' },
+		{ path: "/settings", title: 'Settings', icon: 'fa-solid fa-gear' }
+	])
+</script>
---
+++ src/components/aside/TheMenu.vue
@@ -0,0 +1,27 @@
+<template>
+	<aside class="menu" :class="{ 'close': close }">
+		<button class="menu__close--btn" @click="closeMenu">
+			<i class="fa-solid" :class="{ 'fa-angle-right': close, 'fa-angle-left': !close }"></i>
+		</button>
+
+		<RouterLink to="/">
+			<img :src="menuLogoImg" alt="Logo image" class="menu__logo" />
+		</RouterLink>
+
+		<Naviagation />
+	</aside>
+</template>
+
+<script setup>
+	import { ref, computed } from 'vue'
+	import { RouterLink } from 'vue-router'
+	import LogoImg from '../../assets/images/trivial-logo-light-warm.svg'
+	import LogoImgArrows from '../../assets/images/trivial-logo-arrows-warm.svg'
+	import Naviagation from './Naviagation.vue'
+
+	let close = ref(false)
+
+	const closeMenu = () => close.value = !close.value
+
+	const menuLogoImg = computed(() => close.value ? LogoImgArrows : LogoImg)
+</script>
