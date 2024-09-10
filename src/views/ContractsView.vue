f64d99c503d400e030a3a04a90b378a52612ac8b
GoranRadmanovic <goranradmanovic@gmail.com>
2024-09-10 11:39:50 +0200

    Addressing comments from las review, creating dynamic activity bar chart, fixing issue with filter, default items per page ect.

--- src/views/ContractsView.vue
+++ src/views/ContractsView.vue
@@ -19,7 +19,7 @@
         <div class="flex justify-content-between py-5">
           <div class="flex align-content-center gap-5">
             <h2 class="m-0 line-height-3">Customer Contracts</h2>
-            <Dropdown v-model="selectedContract" :options="dropdownContracts" optionLabel="name" placeholder="Select a Contract" class="w-14rem" />
+            <!--<Dropdown v-model="selectedContract" :options="dropdownContracts" optionLabel="name" placeholder="Select a Contract" class="w-14rem" /> -->
           </div>
           <div>
             <router-link to="/apps/new?paneltype=contract" rel="noopener">
@@ -45,7 +45,7 @@
           <InputText v-else v-model="filterModel.value" type="text" @keydown.enter="filterCallback()" class="p-column-filter" />
         </template>
         <template #filterclear="{ filterCallback }">
-          <Button type="button" @click="() => { filterCallback(); onFilterClear(col.field); }" label="Clear" outlined class="clear-btn" />
+          <Button type="button" @click="() => { filterCallback() }" label="Clear" outlined class="clear-btn" />
         </template>
         <template #filterapply="{ filterCallback }">
           <Button type="button" @click="filterCallback()" label="Apply" severity="success" class="clear-btn" />
@@ -53,62 +53,46 @@
 
         <template #body="{ data, field, index }">
           <span v-if="field === 'descriptive_name'">
-            <router-link :to="`/apps/${data.name}`" rel="noopener">
               {{ data[field] }}
-            </router-link>
           </span>
           <!--<span v-else-if="field === 'created_at'">{{ useFormatDate(data[field]) }}</span>-->
           <span v-else-if="field === 'stats'">
-            <Button type="button" link size="large" text aria-label="Chart Info" @click="toggleActivityPopup(index, $event)" class="p-2">
-              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="20" width="26" class="chart">
-                  <!--<g class="bar" :class="{ 'error-fill': false, 'success-fill': true }" transform="translate(0,0)">
-                    <rect height="10" y="10" width="2"></rect>
+            <template v-if="!data.totalErrors">
+              <i class="pi pi-minus text-color-secondary" />
+            </template>
+            <template v-else>
+              <div class="contract_tooltip">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="20" width="26" class="chart">
+                  <g v-for="(item, index) in data.stats" :key="index">
+                    <rect
+                      :x="index * 4"
+                      :y="20 - item.barHeight"
+                      width="2"
+                      :height="item.barHeight"
+                      :class="{ 'error-fill': item.hasErrors, 'success-fill': !item.hasErrors }"
+                    />
                   </g>
-                  <g class="bar" :class="{ 'error-fill': true, 'success-fill': false }" transform="translate(3,0)">
-                    <rect height="6" y="14" width="2"></rect>
-                  </g>
-                  <g class="bar" :class="{ 'error-fill': false, 'success-fill': true }" transform="translate(6,0)">
-                    <rect height="20" y="4" width="2"></rect>
-                  </g>
-                  <g class="bar" :class="{ 'error-fill': false, 'success-fill': true }"transform="translate(9,0)">
-                    <rect height="10" y="10" width="2"></rect>
-                  </g>
-                  <g class="bar" :class="{ 'error-fill': false, 'success-fill': true }" transform="translate(12,0)">
-                    <rect height="9" y="11" width="2"></rect>
-                  </g>
-                  <g class="bar" :class="{ 'error-fill': false, 'success-fill': true }" transform="translate(15,0)">
-                    <rect height="10" y="10" width="2"></rect>
-                  </g>
-                  <g class="bar" :class="{ 'error-fill': true, 'success-fill': false }" transform="translate(18,0)">
-                    <rect height="17" y="6" width="2"></rect>
-                    
-                  </g>-->
+                </svg>
 
-                  <g v-for="(item, index) in data.stats" :key="index" class="bar mr-1" :class="{ 'error-fill': item.hasErrors, 'success-fill': !item.hasErrors }" :transform="`translate(${index * 3}, 0)`">
-                    <rect :height="barHeight[index]" width="2" :y="`${20 - barHeight[index]}`" />
-                  </g>
-              </svg>
-            </Button>
+                <div class="flex flex-column justify-content-start align-items-center w-11rem h-9rem p-3 shadow-2 border-round-sm bg-white tooltip_content">
+                  <p class="w-full mt-1 mb-2 text-base text-500">Last {{ activityPeriod(data.stats) }} days</p>
+                  <router-link :to="`/apps/${data.name}/activity?search=${activityQueryStrEncoded}`" rel="noopener" class="w-full m-1">
+                    <i class="pi mr-2" :class="{ 'pi-times-circle text-red-600': data.totalErrors > 0, 'pi-check-circle text-primary-500': data.totalErrors == 0 }" /> 
+                    <span class="text-lg font-semibold" :class="{ 'text-red-600': data.totalErrors > 0, 'text-primary-500': data.totalErrors == 0 }">{{ data.totalErrors }} Errors</span>
+                  </router-link>
 
-            <OverlayPanel ref="activityPopup">
-              <div class="flex flex-column justify-content-start align-items-center w-10rem h-8rem">
-                <p class="w-full mt-1 mb-2 text-base text-500">Last {{ activityPeriod(data.stats) }} days</p>
-                <p class="w-full m-1">
-                  <i class="pi mr-2" :class="{ 'pi-times-circle text-red-600': data.totalErrors > 0, 'pi-check-circle text-primary-500': data.totalErrors == 0 }" /> 
-                  <span class="text-lg font-semibold" :class="{ 'text-red-600': data.totalErrors > 0, 'text-primary-500': data.totalErrors == 0 }">{{ data.totalErrors }} Errors</span>
-                </p>
-
-                <Divider />
-                <router-link :to="`/apps/${data.name}/activity?search=${activityQueryStrEncoded}`" rel="noopener" class="w-full flex justify-content-start align-items-center">
-                  <Button label="View Activity" icon="pi pi-external-link" iconPos="right" link class="p-1 text-md font-medium text-blue-500" />
-                </router-link>
+                  <Divider />
+                  <router-link :to="`/apps/${data.name}/activity`" rel="noopener" class="w-full flex justify-content-start align-items-center">
+                    <Button label="View Activity" icon="pi pi-external-link" iconPos="right" link class="p-1 text-md font-medium text-blue-500" />
+                  </router-link>
+                </div>
               </div>
-            </OverlayPanel>
+            </template>
           </span>
           <span v-else>{{ data[field] }}</span>
         </template>
       </Column>
-      <Column class="org-settings__table__action">
+      <Column class="org-settings__table__action text-center">
         <template #body="{ data, field }">
           <router-link v-if="data.canUpdate" :to="`/apps/${data.name}/builder2`" rel="noopener">
             <Button icon="pi pi-pencil" severity="secondary" text rounded aria-label="Edit" />
@@ -126,36 +110,33 @@
   import { useFormatDate } from '@/composable/formatDate.js'
   import { useFilterMatchModes } from '@/composable/filterMatchModes.js'
   import loadingImg from '@/assets/images/trivial-loading-optimized.webp'
-  
+
   const store = useStore(),
     selectedContract = ref(),
-    dropdownContracts = ref([
+    /*dropdownContracts = ref([
       { name: 'All Contract' },
       { name: 'Active' },
       { name: 'Archived' }
-    ]),
+    ]),*/
     selectOrgMsgInfo = 'Please, select an organization.',
     columns = [
       { field: 'descriptive_name', header: 'Customer' },
       //{ field: 'created_at', header: 'Start Date' },
-      { field: 'stats', header: 'Errors' },
+      { field: 'stats', header: 'Activity' },
     ],
     loading = ref(false),
-    rows = ref(10),
+    rows = ref(50),
     rowsPerPageOpt = [10, 20, 50],
     first = ref(0),
     totalRecords = ref(0),
-    barHeight = ref([10, 6, 16, 10, 8, 12, 18]),
+    { dateFilterMatchModes, textFilterMatchModes, defaultMatchMode } = useFilterMatchModes(),
     filters = ref({
-      "created_at": {"operator": "and", "constraints": [{"value": null, "matchMode": "dateIs"}]},
-      "descriptive_name": {"constraints": [{"value": null, "matchMode": "equals"}]}
+      descriptive_name: { value: null, matchMode: defaultMatchMode },
     }),
-    { dateFilterMatchModes, textFilterMatchModes } = useFilterMatchModes(),
-    globalFilterFields = ['descriptive_name', 'created_at'],
+    globalFilterFields = ['descriptive_name'],
     activityPopup = ref([]),
-    activityQueryStr = "[{'c':'status','o':'=','p':'500'}]",
+    activityQueryStr = "[{'name':'status','operator':'=','value':'500'}]",
     activityQueryStrEncoded = encodeURIComponent(JSON.stringify(JSON.parse(activityQueryStr.replace(/'/g, '"'))))
-
   let contracts = ref([]),
     stats = ref([]),
     apps = [],
@@ -222,6 +203,7 @@
         app.stats.forEach(item => {
           item.hasErrors = item?.count.hasOwnProperty('500') ? true : false
           totalErrors += item?.count['500'] || 0
+          item.barHeight = ((item?.count['500'] || 0) + (item?.count['200'] || 0)) || 1 // Default bar chart hight is 1px
         })
 
         app.totalErrors = totalErrors
@@ -235,30 +217,8 @@
     //return apps
   }
 
-  const randomIntFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
   const activityPeriod = item => item.length
   const getTotalErrors = item => item.totalErrors
-  const toggleActivityPopup = (index, event) => activityPopup.value[index].toggle(event)
   const totalPaginatorPages = (totalItems, itemsPerPage) => totalItems / itemsPerPage
   const setFilterMatchModes = field => field === 'created_at' ? dateFilterMatchModes : textFilterMatchModes
-  const onFilterClear = async field => {
-    /*loading.value = true
-
-    if (filters.value.hasOwnProperty(field)) {
-      setDefaultFilters(field)
-    }
-
-    try {
-      page = 1
-      const queryString = updateQueryString()
-      const { register_items } = await getRegistersData(queryString)
-      registers.value = register_items
-      // await getTotalAmount()
-      loading.value = false
-    } catch (err) {
-      console.error(err)
-    }
-
-    loading.value = false*/
-  }
 </script>
