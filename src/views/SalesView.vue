d661270fcd998d50556ad3714c5f07c820c93171
GoranRadmanovic <goranradmanovic@gmail.com>
2024-09-17 14:39:01 +0200

    Finisihng UI changes for CSV export dialog

--- src/views/SalesView.vue
+++ src/views/SalesView.vue
@@ -33,7 +33,7 @@
 	        <div class="flex justify-content-between py-5">
 	        	<h2 class="m-0">Revenue Detail</h2>
 
-	        	<Button label="Export as CSV" aria-label="Download CSV" icon="pi pi-download" @click="exportCSV" />
+	        	<Button label="Export as CSV" aria-label="Download CSV" icon="pi pi-download" class="registers_table--csv-btn" @click="exportCSV" />
 
 	        	<!-- Global keyword search filed -->
 	            <!--<IconField iconPosition="right">
@@ -97,40 +97,72 @@
 		</ColumnGroup>
 	</DataTable>
 
-	<Dialog v-model:visible="csvDialogVisible" modal :closable="false">
+	<Dialog v-model:visible="csvDialogVisible" modal :closable="false" class="csv__dialog">
 		<div class="flex align-items-center justify-content-between gap-3 mb-3">
-			<template v-if="streamPending || streamOpen">
+			<template v-if="(streamPending || streamOpen) && !cancelCSV"> <!-- streamPending || streamOpen-->
 				<h3 class="m-0 text-lg font-normal">Generating CSV...</h3>
 				<span class="text-lg font-normal text-600">{{ streamProgress }}%</span>
 			</template>
+
+			<template v-if="cancelCSV">
+				<h3 class="m-0 text-lg font-normal">Cancel Export</h3>
+			</template>
+
 			<template v-if="streamFailed">
-				<h3 class="m-0 text-lg font-normal">Failed to Generate CSV!</h3>
+				<h3 class="m-0 text-lg font-normal">Failed to Generate CSV</h3>
 			</template>
 		</div>
-		<div v-if="streamPending || streamOpen" class="flex align-items-center justify-content-center mb-1">
-			<ProgressBar :value="streamProgress" aria-label="CSV Progress Download Status" class="w-full h-1rem" />
+		<div v-if="(streamPending || streamOpen) && !cancelCSV" class="flex align-items-center justify-content-center mb-2"> <!-- streamPending || streamOpen-->
+			<ProgressBar :value="streamProgress" aria-label="CSV Progress Download Status" class="w-full h-1rem csv__dialog--progersbar" />
 		</div>
-		<div  class="flex gap-2 align-items-center justify-content-end mb-3 text-sm text-600">
+
+		<div class="flex align-items-center mb-4 text-sm text-600">
 			<template v-if="streamPending">
-				Preparing your download now...
+				<p class="m-0">Preparing your download now...</p>
 			</template>
-			<template v-if="streamOpen">
-				Generated {{ streamedLines }} out of {{ streamedLinesTotal }} rows
+
+			<template v-if="streamOpen && !cancelCSV"> <!--streamOpen -->
+				<p class="w-full m-0 text-right">Generated <span class="csv__dialog--streamed-lines">{{ streamedLines }}</span> out of {{ streamedLinesTotal }} rows</p>
 			</template>
+
+			<template v-if="cancelCSV">
+				<p class="m-0">Are you sure you want to cancel the export?</p>
+			</template>
+
 			<template v-if="streamFailed">
-				Error: {{ streamErrorMessage }}
+				<p class="m-0 text-base text-red-500">Error: {{ streamErrorMessage }}!</p>
 			</template>
 		</div>
+
 		<div class="flex align-items-center justify-content-between gap-1">
-			<i class="pi pi-exclamation-triangle text-600" />
-			<template v-if="streamPending || streamOpen">
-				<p class="text-xs text-600">This might take some time. Closing this page will cancel the download.</p>
-				<Button type="button" label="Cancel" text class="text-gray-900" @click="cancelStream()" />
+			<template v-if="(streamPending || streamOpen) && !cancelCSV"> <!-- streamPending || streamOpen-->
+				<p class="text-xs text-600">
+					<i class="pi pi-exclamation-triangle text-600" />
+					This might take some time. Closing this page will cancel the download.
+				</p>
+				<Button type="button" label="Cancel" text class="csv__dialog--cancel-btn" @click="csvExportShow()" />
 			</template>
+
+			<template v-if="cancelCSV">
+				<Button type="button" label="Stop Export" text class="csv__dialog--cancel-btn" @click="cancelStream()" />
+				<Button type="button" label="Keep the Export Running" class="csv__dialog--btn" @click="csvExportHide()" />
+			</template>
+
 			<template v-if="streamFailed">
-				<p class="text-xs text-600">Please close this dialogue to try again</p>
-				<Button type="button" label="Cancel" text class="text-gray-900" @click="resetStreamInfo()" />
+				<Button type="button" label="OK" class="csv__dialog--btn" @click="resetStreamInfo(), closeCSVDialog()" />
 			</template>
+		</div>
+
+		<div v-if="streamClosed" class="flex flex-column align-items-center justify-content-center gap-2">
+			<Icon icon="fa6-solid:circle-check" class="w-3rem h-3rem text-primary" />
+			<h3 class="m-0 text-lg font-normal font-semibold">Success</h3>
+
+			<p class="flex align-items-center text-sm text-600 gap-1">
+				<i class="pi pi-check-circle text-green-500" />
+				CSV Export completed.
+			</p>
+
+			<Button type="button" label="OK" class="csv__dialog--btn" @click="resetStreamInfo(), closeCSVDialog()" />
 		</div>
 	</Dialog>
 </template>
@@ -146,35 +178,37 @@
 	import { useFilterMatchModes } from '@/composable/filterMatchModes.js'
 	import loadingImg from '@/assets/images/trivial-loading-optimized.webp'
 	import { useToastNotifications } from '@/composable/toastNotification'
+	import { Icon } from '@iconify/vue'
 
 	const loading = ref(false),
-			filters = ref({}),
-			registers = ref([]),
-			totalAmount = ref(null),
-			rows = ref(10), // per_page
-			totalRecords = ref(0),
-			first = ref(1),
-			totalInfoPopup = ref(),
-			sortField = ref(null),
-			sortOrder = ref(null),
-			rowsPerPageOpt = [10, 20, 50],
-			{dateOptions, timeOptions, timeZoneOptions} = useDateTimeZoneOptions(),
-			{
-				dateFilterMatchModes,
-				numericFilterMatchModes,
-				textFilterMatchModes,
-				filterMatchModeMapping,
-				defaultFilters,
-				defaultMatchMode,
-				globalFilterFields
-			} = useFilterMatchModes(),
-			{ showSuccessToast, showErrorToast, showInfoToast } = useToastNotifications(),
-			store = useStore(),
-			registersNames = ['Sales', 'Income Account'],
-			selectOrgMsgInfo = 'Please, select an organization.',
-			timezone = timeZoneOptions.timeZone
-			csvDialogVisible = ref(false),
-            streamErrorMessage = ref('')
+		filters = ref({}),
+		registers = ref([]),
+		totalAmount = ref(null),
+		rows = ref(10), // per_page
+		totalRecords = ref(0),
+		first = ref(1),
+		totalInfoPopup = ref(),
+		sortField = ref(null),
+		sortOrder = ref(null),
+		rowsPerPageOpt = [10, 20, 50],
+		{dateOptions, timeOptions, timeZoneOptions} = useDateTimeZoneOptions(),
+		{
+			dateFilterMatchModes,
+			numericFilterMatchModes,
+			textFilterMatchModes,
+			filterMatchModeMapping,
+			defaultFilters,
+			defaultMatchMode,
+			globalFilterFields
+		} = useFilterMatchModes(),
+		{ showSuccessToast, showErrorToast, showInfoToast } = useToastNotifications(),
+		store = useStore(),
+		registersNames = ['Sales', 'Income Account'],
+		selectOrgMsgInfo = 'Please, select an organization.',
+        timezone = timeZoneOptions.timeZone,
+		streamErrorMessage = ref(''),
+		cancelCSV = ref(false),
+		csvDialogVisible = ref(false)
 
 	let columns = [],
 		defaultColumns = [
@@ -195,15 +229,14 @@
 	const register = computed(() => store.getters.getRegister)
 	const registersItems = computed(() => registers.value)
 	const streamStatus = computed(() => store.getters.getStreamStatus)
-	const streamProgress = computed(() => Math.floor((streamedLines.value / streamedLinesTotal.value) * 100))
+	const streamProgress = computed(() => parseInt(Math.floor((streamedLines.value / streamedLinesTotal.value) * 100)))
 	const streamedLines = computed(() => store.getters.getStreamedLines)
 	const streamedLinesTotal = computed(() => store.getters.getStreamedLinesTotal)
 	const streamPending = computed(() => streamStatus.value === 'pending')
 	const streamOpen = computed(() => streamStatus.value === 'open')
+	const streamClosed = computed(() => streamStatus.value === 'closed')
 	const streamFailed = computed(() => streamStatus.value === 'failed')
-	const csvDialogVisible = computed(() => {
-		return streamPending.value || streamOpen.value || streamFailed.value
-	})
+	//const csvDialogVisible = computed(() => streamPending.value || streamOpen.value || streamFailed.value)
 
 	watch(orgId, async (newVal, oldVal) => {
 		if (!newVal) {
@@ -212,18 +245,16 @@
 			resetRegisters()
 			return
 		}
-
-		console.log('newVal - ', newVal)
 		await getRegisters(newVal)
 	})
 
 	onMounted(async () => {
 		let organizationId = storageOrgId || orgId.value
 
-		console.log('organizationId - ', organizationId)
 		if (organizationId) {
 			await getRegisters(orgId.value)
 		}
+
 		if (localStorage.getItem('orgId') === 'null') {
 			showInfoToast('Info', selectOrgMsgInfo, 3000)
 		}
@@ -241,10 +272,18 @@
 	const isDisabledTooltip = data => data?.length < 14
 	const toggleTotalInfoPopup = event => totalInfoPopup.value[0].toggle(event)
 	const setFilterMatchModes = field => field === 'amount' ? numericFilterMatchModes : field === 'originated_at' ? dateFilterMatchModes : textFilterMatchModes
-	const dateToISOString = date => new Date(date).toISOString()
-	const dateSetFullYear = date => new Date().setFullYear(date)
-	const cancelStream = () => store.commit('setStreamStatus', 'cancelling')
+	//const dateToISOString = date => new Date(date).toISOString()
+	//const dateSetFullYear = date => new Date().setFullYear(date)
+	const cancelStream = () => { 
+		store.commit('setStreamStatus', 'cancelling')
+		csvExportHide()
+		closeCSVDialog()
+	}
 	const resetStreamInfo = () => store.dispatch('resetStreamInfo')
+	const csvExportShow = () => cancelCSV.value = true
+	const csvExportHide = () => cancelCSV.value = false
+	const openCSVDialog = () => csvDialogVisible.value = true
+	const closeCSVDialog = () => csvDialogVisible.value = false
 
 	const getRegisters = async orgId => {
 		loading.value = true
@@ -491,7 +530,9 @@
 	}
 
 	const exportCSV = async () => {
+		openCSVDialog()
 		store.commit('setStreamStatus', 'pending')
+
 		try {
 			let csvData = await store.state.Session.apiCall(
 				`/register_items.csv?register_id=${regId.value}&${queryString.value}`,
@@ -500,9 +541,11 @@
 				'csv',
 				true
 			)
+
 			if (csvData) {
 				forceFileDownload(csvData, register.value.name)
 			}
+
 			resetStreamInfo()
 		} catch (err) {
 			console.log(err)
@@ -515,7 +558,8 @@
 		const url = window.URL.createObjectURL(
 			new Blob([response]),
 			{type: 'text/csv;charset=utf-8'}
-		), link = document.createElement('a')
+		),
+		link = document.createElement('a')
 
 		link.href = url
 		link.setAttribute('download', `${title}.csv`)
